c83cfcf8b509d40756fc1bf6a7f80c2d103cf60b
cypa8074 <cy.parker@colorado.edu>
2016-03-14 11:36:52 -0600

    added password hasher

---
+++ dictionaryattack/Hasher.java
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////
+//	Hasher.java
+//
+//	Hasher takes a string and hash algorithm and finds the
+//	hash of the string using that hash algorithm.
+//
+///////////////////////////////////////////////////////////
+
+import java.security.MessageDigest;
+import javax.xml.bind.DatatypeConverter;
+import java.security.NoSuchAlgorithmException;
+
+public class Hasher{
+
+	public String password;
+	public String password_hash
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param string   password string to store and hash
+	 * @param hash_algorithm  hash algorithm to use
+	 */
+	public Hasher(String string, String hash_algorithm) {
+	
+		password = passwordString;
+		hash(hash_algorithm);
+	}
+	
+	/**
+	 * Hashes the password using the given hash_algorithm
+	 * @param String hash_algorithm hash algorithm to use
+	 */
+	void hash(String hash_algorithm){
+		
+		MessageDigest hasher = null;
+		
+		try {
+			hasher = MessageDigest.getInstance(hash_algorithm);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} 
+		
+		//MessageDigest takes in a byte array
+		hasher.update(originalString.getBytes());
+		// Computes the hash and then converts the hashed byte array to a (lower case) string
+		password_hash = DatatypeConverter.printHexBinary(hasher.digest()).toLowerCase();
+	}
+}
